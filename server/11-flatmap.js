c2f08269882f4077c7cb0dc3b2a5b7a021b2f3f7
Gustavo Rodriguez <gustavojrs01@gmail.com>
2023-04-03 15:59:32 -0400

    fin del curso

---
+++ server/11-flatmap.js
@@ -0,0 +1,31 @@
+const calendars = {
+    primaryCalendar: [
+      {
+        startDate: new Date(2021, 1, 1, 15),
+        endDate: new Date(2021, 1, 1, 15, 30),
+        title: "Cita 1",
+      },
+      {
+        startDate: new Date(2021, 1, 1, 17),
+        endDate: new Date(2021, 1, 1, 18),
+        title: "Cita 2",
+      },
+    ],
+    secondaryCalendar: [
+      {
+        startDate: new Date(2021, 1, 1, 12),
+        endDate: new Date(2021, 1, 1, 12, 30),
+        title: "Cita 2",
+      },
+      {
+        startDate: new Date(2021, 1, 1, 9),
+        endDate: new Date(2021, 1, 1, 10),
+        title: "Cita 4",
+      },
+    ],
+  }
+
+  
+const fechas = Object.values(calendars).flatMap(fecha=>fecha.map(date=>date.startDate))
+
+console.log(fechas)
